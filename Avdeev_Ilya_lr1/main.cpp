1b76cb20b780b0984eecd57c98f181ed5ae793e1
DerzoMelon <49362112+DerzoMelon@users.noreply.github.com>
2019-10-11 00:45:06 +0400

    lr1

---
+++ Avdeev_Ilya_lr1/main.cpp
@@ -0,0 +1,35 @@
+﻿#include "pch.h"
+#include <iostream>
+#include "fstream"
+using namespace std;
+
+int Bin(int n, int m)
+{
+	if (n == m)
+		return 1;
+	if (m == 1)
+		return n;
+	return Bin(n - 1, m - 1) + Bin(n - 1, m);
+}
+
+int main()
+{
+	ofstream lof;
+	ifstream lin;
+	int m;
+	int n;
+	lin.open("lin.txt");
+	lin >> n;
+	lin >> m;
+	lin.close();
+	if (n < 0 | m < 0)
+		cout << "Numbers must be positive";
+	else 
+	{
+		lof.open("lof.txt");
+		cout << Bin(n, m);
+		lof << Bin(n, m);
+		lof.close();
+	}
+	return 0;
+}
