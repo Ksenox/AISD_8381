b8ac885adaba10d99dbd908266ff172f3c75a1a6
GandhiKK <gandhi.noir@mail.ru>
2019-10-24 01:29:20 +0300

    only code

---
+++ Kireev_Konstantin_lr3/incremet.cpp
@@ -0,0 +1,6 @@
+#include "incremet.h"
+
+Incremet::Incremet()
+{
+
+}
---
+++ Kireev_Konstantin_lr3/incremet.h
@@ -0,0 +1,11 @@
+#ifndef INCREMET_H
+#define INCREMET_H
+
+
+class Incremet
+{
+public:
+    Incremet();
+};
+
+#endif // INCREMET_H
